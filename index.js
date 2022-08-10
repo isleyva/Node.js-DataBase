a302fc419ca5a6b77bfeb08217a11ada5bf6c4cc
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-11 00:21:34 +0200

    adding body-parser and port

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 "use strict"
 
 const express = require("express") 
+const bodyParser = require("body-parser")
 
 const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.listen(3000, () => {
+    console.log(`Server is running on port ${port}`)
+} )
