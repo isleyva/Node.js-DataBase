4385555467138411ae15ef15d92028e6d262d389
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-16 18:46:05 +0200

    connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require("express") 
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -27,6 +28,14 @@ app.put("/api/product/:productID", (req, res) => {})
 
 app.delete("/api/product/:productID", (req, res) => {})
 
-app.listen(3001, () => {
-    console.log(`Server is running on port ${port}`)
+mongoose.connect("mongodb://localhost:27017", (err) => {
+    if (err)  {
+        return console.log(`error al conectar a la base de datos: ${err}`)
+    }
+    console.log("Conexión a la base de datos establecida")
+
+    app.listen(port, () => {
+        console.log(`API REST corriendo en http://localhost:${port}`)
+    } )
 } )
+
