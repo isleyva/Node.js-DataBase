9a6f33f89f766567ca9a634e138b937aba4f45a0
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-08-17 16:53:19 +0200

    product schema function

--- models/product.js
+++ models/product.js
@@ -1,5 +1,14 @@
 "use strict"
 
-
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+
+const productSchema =  Schema({
+    name: String,
+    price: {type: Number, default: 0},
+    description: String,
+    picture: String,
+    category: { type: String, enum: ["computers", "phones", "accesories"] },
+})
+
+const Product = mongoose.model("Product", productSchema)
